ab1b5749441efb029d337e6cbb3304e6ecf1d03d
Sergio-hdD <sergio-hdd@hotmail.com>
2020-05-07 14:18:20 -0300

    en base a lo hecho el Martes, completé carga, muestra, pre y post

--- Lectura.cpp
+++ Lectura.cpp
@@ -1,6 +1,10 @@
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <bits/stdc++.h>
 #include "Lectura.h"
-#include "Figuras.h"
-#include <list>
+
 using namespace std;
 
 
@@ -18,31 +22,47 @@ string replaceNum(string s){
     return s;
 }
 
-void separarPalabras(string s,string* &puntero)
+void separarPalabras(string s, t_lectura *&lectura_delinea)
 {
     string aux;
     std::string delimiter = " ";
     size_t posDelimiter = 0;
-    int i=0;
 
-    //ver info de npos http://www.cplusplus.com/reference/string/string/npos/
+    t_lectura *nueva_lec =  new t_lectura();
+    int i=0;
     while ((posDelimiter = s.find(delimiter)) != std::string::npos)//mientras la posicion del espacio (delimiter) no sea la última de la linea
     {
-        puntero[i]= s.substr(0, posDelimiter);//guardo en geeks[i] los caracteres desde 0 la cantidad de "pos" caracteres
+        nueva_lec->p[i] = s.substr(0, posDelimiter);//guardo en geeks[i] los caracteres desde 0 la cantidad de "pos" caracteres
         s.erase(0, posDelimiter + delimiter.length());//Elimina del vector un solo elemento (posición) o un rango de elementos([desde, hasta]), este caso es el segundo de modo que en la vuelta siguiente la posicion 0 va a ser donde está el espacio ahora.
-        i++;
+    i++;
     }
-    puntero[i]=s;
+
+    nueva_lec->p[i] = s;
+    //PARA ORDENAR:
+    t_lectura *aux1 = lectura_delinea;//1 creo una lista auxiliar y guardo los datos de lista
+    t_lectura *aux2;//creo una lista auxiliar
+    while(aux1 != NULL){//mientras la lista tenga algo y el input_id sea mayor a los que hay en la lista
+        aux2 = aux1;// aux2 pasa contener lo del primer nodo que encuentro en la lista y tambien aux2 va a apuntar a lo que apuntaba el primer nodo que encuentro en la lista
+        aux1 = aux1->siguiente;// paso a la siguiente posición (al siguiente nodo)
+        //al salir del while tengo un nuevo orden en aux2
+     }
+
+   if(lectura_delinea == aux1){//si no entra al while
+     lectura_delinea = nueva_lec;//2  los datos del nuevo nodo los guardo en la lista y el siguente de lista apunta a nuevo nodo
+   }
+    else{//si entró (entonces  mi lista tiene un nuevo orden, y esta en aux2)
+       aux2->siguiente = nueva_lec; //el nuevo nodo va a ser el siguiente todo lo que haya en aux2
+   }
+
+   nueva_lec->siguiente = aux1; //entre o no al while, todo lo que haya en aux1 va a ser lo siguiente de nuevo nodo
+//NOTA: si entró al whille la lista queda  aux2 ---> nueva_lec --> aux1  Y  si NO entró al whille la lista queda nueva_lec --> aux1
+
 }
 
-
-void Lectura(t_figura *&o_figura)
+void Lectura(t_lectura *&o_lectura)
 {
     ifstream archivo;//ifstream(tipo de variable para abrir un archivo)...  archivo (nombre de la variable)
     string texto;// variable string
-    string* puntero=new string[5];
-//    list<string> listaString;
-    int id=0;
     archivo.open("Figuras.txt",ios::in);// con archivo.open le digo que quiero abrir un archivo (el archivo "FigurasEjemplo.txt") y con ios::in le digo que abro para leerlo
 
     if(archivo.fail())//si hay un error y no se abre el arvhivo
@@ -50,59 +70,28 @@ void Lectura(t_figura *&o_figura)
         cout<<"No se ha podido abrir archivo"<<endl;//se muestra
         exit(1);//sale
     }
-
      //si se abre
     while(!archivo.eof())//mientras no sea el final del archivo
-    {  id++;
+    {
         getline(archivo,texto);//Tomo lo que va encontrando (por linea, creo) en "archivo" y lo copio en "texto"
-        separarPalabras(texto,puntero);//Mando un puntero a un vector y toda lo copiado (la linea) a separarlo (para guardar en las distintas posiciones del vector)
-        buscarFigura(id, puntero, o_figura);
+        separarPalabras(texto, o_lectura);
     }
       archivo.close();//cerramos archivo
 
 }
 
-void buscarFigura(int id, string *&p, t_figura *&o_figura)
-{
-        string tipo; string color; string valor1; string valor2;
-        for(int i=0;i<5;i++)
-        {
-            switch(i) {
-              case 0:tipo = *p;break;
-              case 1:color = *(p+i);break;
-              case 2:valor1 = *(p+i);break;
-              case 3:valor2 = *(p+i);break;
-              }
+void mostrarLista (t_lectura *&ptrLectura){
+t_lectura *actual=new t_lectura();
+actual=ptrLectura;
 
-        }
-       llamarContructorDeLista(id, tipo, color, valor1, valor2, o_figura);
-}
+	while(actual != NULL)
+	{
+		cout<<endl<<" Tipo: "<<actual->p[0]
+		<<", color: "<<actual->p[1]<<", valor: "<<actual->p[2]
+		<<", valor2: "<<actual->p[3]<<endl;
 
-void llamarContructorDeLista(int id, string tipo, string color, string valor1, string valor2, t_figura *&o_figura){
-       string descripcion;
-       float valor1f = convertirStringA_float(replaceNum(valor1));
-       float valor2f = convertirStringA_float(replaceNum(valor2));
-
-       if(tipo=="circulo"){
-         descripcion = "Radio: "+valor1;
-        constructor( id, valor1f, tipo, color, descripcion, o_figura);
-       }
-       if(tipo =="cubo"){
-        descripcion = "Lado: "+valor1;
-        constructor( id, valor1f, tipo, color, descripcion, o_figura);
-       }
-        if(tipo =="cilindro"){
-        descripcion = "Radio: "+valor1+" Altura: "+valor2;
-        constructor( id, valor1f+valor2f, tipo, color, descripcion, o_figura);
-       }
-       if(tipo =="rectangulo"){
-        descripcion = "Base: "+valor1+" Altura: "+valor2;
-        constructor( id, valor1f+valor2f, tipo, color, descripcion, o_figura);
-       }
-       if(tipo =="triangulo"){
-        descripcion = "Base: "+valor1+" Altura: "+valor2;
-        constructor( id, areaTriangulo(valor1f,valor2f), tipo, color, descripcion, o_figura);
-       }
+		actual=actual->siguiente;
+	}
 }
 
 float convertirStringA_float(string variableString){
